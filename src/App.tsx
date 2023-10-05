d123553069151956ff2790685795287a76ebf208
Wellici Araujo <wja1@ifal.edu.br>
2023-10-05 15:50:48 -0300

    :bug: login

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import Register from "./pages/Register";
 export default function App() {
   return (
     <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<NotFound />} />
